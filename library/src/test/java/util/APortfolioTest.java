7870c65c4950c89ee5942b5b67f74e121b3c7eeb
Jeff L <jeff@langrsoft.com>
2022-05-09 15:38:19 -0600

    mob next [ci-skip] [ci skip] [skip ci]

--- library/src/test/java/util/APortfolioTest.java
+++ library/src/test/java/util/APortfolioTest.java
@@ -4,37 +4,44 @@ package util;
 
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.Matchers.equalTo;
 
 class APortfolioTest {
     private Portfolio portfolio = new Portfolio();
 
-    @Test
-    void answersUniqueSymbolCountAsZero_WhenNoPurchasesMode() {
-        assertThat(portfolio.uniqueSymbolCount(), equalTo(0));
+    @org.junit.jupiter.api.Test
+    public void x() {
+        assertEquals(true, false);
     }
 
-    @Test
-    public void answersUniqueSymbolCountAsToOne_AfterPurchaseMade() {
-        portfolio.buy("ZBRA");
-
-        assertThat(portfolio.uniqueSymbolCount(), equalTo(1));
-    }
-
-    @Test
-    public void incrementsUniqueSymbolCount_WithEachNewSymbolPurchased() {
-        portfolio.buy("ZBRA");
-        portfolio.buy("AAPL");
-
-        assertThat(portfolio.uniqueSymbolCount(), equalTo(2));
-    }
-
-    @Test
-    public void doesNotIncrementUniqueSymbolCount_WhenSameSymbolPurchased() {
-        portfolio.buy("ZBRA");
-        portfolio.buy("ZBRA");
-
-        assertThat(portfolio.uniqueSymbolCount(), equalTo(1));
-    }
+//    @Test
+//    void answersUniqueSymbolCountAsZero_WhenNoPurchasesMode() {
+//        assertThat(portfolio.uniqueSymbolCount(), equalTo(0));
+//    }
+//
+//    @Test
+//    public void answersUniqueSymbolCountAsToOne_AfterPurchaseMade() {
+//        portfolio.buy("ZBRA");
+//
+//        assertThat(portfolio.uniqueSymbolCount(), equalTo(1));
+//    }
+//
+//    @Test
+//    public void incrementsUniqueSymbolCount_WithEachNewSymbolPurchased() {
+//        portfolio.buy("ZBRA");
+//        portfolio.buy("AAPL");
+//
+//        assertThat(portfolio.uniqueSymbolCount(), equalTo(2));
+//    }
+//
+//    @Test
+//    public void doesNotIncrementUniqueSymbolCount_WhenSameSymbolPurchased() {
+//        portfolio.buy("ZBRA");
+//        portfolio.buy("ZBRA");
+//
+//        assertThat(portfolio.uniqueSymbolCount(), equalTo(1));
+//    }
 }
